f3c014cb79196da74cf3137fbd4cb4728cc0fc47
kappy <kapeeshmanilal@gmail.com>
2020-11-08 20:11:03 +0100

    add modules and test lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'aventador',
+    loadChildren: () => import('./aventador/aventador.module').then(m => m.AventadorModule)
+  }, {
+    path: 'huracan',
+    loadChildren: () => import('./huracan/huracan.module').then(m => m.HuracanModule)
+  }, {
+    path: 'urus',
+    loadChildren: () => import('./urus/urus.module').then(m => m.UrusModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
