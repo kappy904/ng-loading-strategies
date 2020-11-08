45e65b5a3abb51e4f061e705c9285652394e875c
kappy <kapeeshmanilal@gmail.com>
2020-11-08 20:54:03 +0100

    merge formatting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: 'aventador',
+    loadChildren: () => import('./aventador/aventador.module').then((m) => m.AventadorModule)
+  },
+  {
+    path: 'huracan',
+    loadChildren: () => import('./huracan/huracan.module').then((m) => m.HuracanModule)
+  },
+  {
+    path: 'urus',
+    loadChildren: () => import('./urus/urus.module').then((m) => m.UrusModule)
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
